17f846d7f45587261d48abb411db1d9c705b3e7c
kss418 <jis1595@naver.com>
2026-03-20 18:10:18 +0900

    Round 1041 추가

---
+++ Div.1 + Div.2/1000/1041/B_Hamiiid_Haaamid_Hamid.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+void run(){
+    cin >> n >> m >> s;
+    if(m == 1 || m == n){ cout << 1 << "\n"; return; }
+
+    ll l = 0, r = n + 1;
+    for(int i = m - 2;i >= 0;i--){
+        if(s[i] != '#') continue;
+        l = i + 1; break;
+    }
+
+    for(int i = m;i < n;i++){
+        if(s[i] != '#') continue;
+        r = i + 1; break;
+    }
+
+    cout << max(min(l, n - m), min(m - 1, n - r + 1)) + 1 << "\n";
+}
+
+int main() {
+    fastio; cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
